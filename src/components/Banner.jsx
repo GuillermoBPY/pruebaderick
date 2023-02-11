0789b7477c642f472b138611907e701145ac81d4
GuillermoBPY <guillermo-benitez@outlook.com>
2023-02-11 09:12:59 -0300

    Actualizado

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
-import ParticleHeader from '../Particles/ParticleHeader'
-import ParticleMenu from '../Particles/ParticleMenu'
+import React, { useState } from "react";
+import VideoBgHeader from "./VideoBgHeader";
 
 const Banner = () => {
-  const [showmenu, setshowmenu] = useState("notshowmenu")
+  const [showmenu, setshowmenu] = useState("notshowmenu");
 
-const handleMenuBtn = () => {
-  showmenu === "notshowmenu" ? setshowmenu("showmenu") :setshowmenu("notshowmenu")
-}
+  const handleMenuBtn = () => {
+    showmenu === "notshowmenu"
+      ? setshowmenu("showmenu")
+      : setshowmenu("notshowmenu");
+  };
 
   return (
     <header className="header">
-      
       <figure className="header__figure">
         <img src="../../img/ramfy.png" alt="" />
       </figure>
@@ -20,21 +20,24 @@ const handleMenuBtn = () => {
       </figure>
 
       <nav className="header__nav">
-        <button onClick={handleMenuBtn} className={`header__nav--btn ${showmenu}`}>
+        <button
+          onClick={handleMenuBtn}
+          className={`header__nav--btn ${showmenu}`}
+        >
           <span></span>
           <span></span>
           <span></span>
         </button>
         <ul className={`header__nav--ul ${showmenu}`}>
-          <ParticleMenu />
+          <VideoBgHeader />
           <li>Characters</li>
           <li>Episodes</li>
           <li>Locations</li>
         </ul>
       </nav>
-      <ParticleHeader />
+      <VideoBgHeader />
     </header>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
---
+++ src/components/VideoBg.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const VideoBg = () => {
+  return (
+    <video
+      autoPlay
+      loop
+      muted
+      style={{
+        position: "fixed",
+        width: "100%",
+        left: "50%",
+        top: "50%",
+        height: "100%",
+        objectFit: "cover",
+        transform: "translate(-50%, -50%)",
+        zIndex: "-1",
+        overflow: "hidden",
+      }}
+    >
+      <source src="../video/fondo.mp4" type="video/mp4" />
+    </video>
+  );
+};
+
+export default VideoBg;
